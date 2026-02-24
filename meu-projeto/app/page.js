4e40f2beadceb27095d65a90740a4b0cf88c04ca
GetsG <getuliogo65@gmail.com>
2026-02-24 14:00:40 -0300

    Front tela de login criada.

--- meu-projeto/app/page.js
+++ meu-projeto/app/page.js
@@ -1,46 +1,76 @@
 "use client";
+import Image from 'next/image'
 import { useForm } from "react-hook-form";
-import Image from "next/image";
+import styles from './page.module.css'
+import Logo from '../public/logo.png';
+import { Lock } from "@deemlol/next-icons";
+import { Mail } from "@deemlol/next-icons"
 
 export default function Home() {
 
   const {register, handleSubmit, formState: {errors}} = useForm()
 
+  {/* Retorno do formulário, email e senha */}
   const onSubmit = (data) => {
     console.log('data', data)
   }
 
   return (
-    <div>
-        <div>
-          <h1>Denuncie Chamas</h1>
-          <p>Entre com sua conta para registrar denúncias de incêndios</p>
-        </div>
+    <div className={styles.container}>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.card}>
+
+        {/* Logo, Titulo e Subtitulo */}
+        <div className={styles.cardTitle}>
+          <Image className={styles.logo} src={Logo}/>
+          <h1 className={styles.title}>Denuncie Chamas</h1>
+          <p className={styles.subTitle}>Entre com sua conta para registrar denúncias de incêndios</p>
+        </div>
+        {/* ------------------------------------ */}
+
+        {/* Formulário Email e Senha*/}
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+
+          {/*Email*/}
             <div>
-                <label>Email:</label>
-                <input type="email"
-                {...register('email', { required: "Email obrigatório"})}/>
+                <label>Email</label>
+
+                <div className={styles.inputWrapper}>
+                  <span className={styles.icon}><Mail size={20}/></span>
+                  <input className={styles.input} type="email" placeholder="seu@email.com"
+                  {...register('email', { required: "Email obrigatório"})}/> 
+                </div>
+                
                 {errors.email && <p>{errors.email.message}</p>}
             </div>
-
+          {/* ------------------------------------ */}
+            
+        {/* Senha */}
             <div>
-                <label>Senha:</label>
-                <input type="password"
+                <label>Senha</label>
+
+              <div className={styles.inputWrapper}>
+                <span className={styles.icon}><Lock size={20} /></span>
+                <input className={styles.input} type="password" placeholder="********"
                 {...register('password', { required: "Senha obrigatória", minLength:{value: 8, message: "Mínimo 8 caracteres"} })}/>
+              </div>
+                      
                 {errors.password && <p>{errors.password?.message}</p>}
             </div>
+        {/* ------------------------------------ */}
 
+        {/* Botão Entrar */}
             <button type="submit">
                 Entrar
             </button>
-
-            
-            
+        {/* ------------------------------------ */}
         </form>
-        
 
+        {/* Registrar */}
+        <p className={styles.register}>Não tem uma conta? <span> Cadastre-se </span></p>
+        {/* ------------------------------------ */}
+
+      </div>
 
     </div>
   );
