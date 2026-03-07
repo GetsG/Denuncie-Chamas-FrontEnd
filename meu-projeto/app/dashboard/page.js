c81b5eb6dbf26e7bbd93a43a4ea954e6b96c08a6
GetsG <getuliogo65@gmail.com>
2026-03-07 11:27:22 -0300

    Icones adicionados a tela de dashboard

--- meu-projeto/app/dashboard/page.js
+++ meu-projeto/app/dashboard/page.js
@@ -3,6 +3,23 @@ import { useEffect } from "react"
 import { useState } from "react"
 import styles from "./page.module.css"
 
+import { FileText } from "@deemlol/next-icons";
+//<FileText size={24} color="#FFFFFF" /> MINHAS DENUNCIAS
+
+import { PlusCircle } from "@deemlol/next-icons";
+//<PlusCircle size={24} color="#FFFFFF" /> NOVA DENUNCIA
+
+import { BarChart2 } from "@deemlol/next-icons";
+//<BarChart2 size={24} color="#FFFFFF" /> TOTAL DENUNCIAS
+
+import { AlertTriangle } from "@deemlol/next-icons"
+//<AlertTriangle size={24} color="#FFFFFF" />
+
+
+import Brightness1Icon from '@mui/icons-material/Brightness1';
+
+
+
 export default function Dashboard(){
 
     //VERIFICAR SE O TOKEN ESTÁ ARMAZENADO NO STORAGE
@@ -31,33 +48,40 @@ export default function Dashboard(){
                 <section>
                     <h4>Total de Denúncias</h4>
                     <p>{total}</p>
+                    <BarChart2 size={25} color="#3f7cd8" />
                 </section>
 
                 <section>
                     <h4>Pendentes</h4>
                     <p>{pendentes}</p>
+                    <Brightness1Icon className={styles.iconPendente} sx={{ fontSize: 25 , color: '#F0B100'}}/>
                 </section>
 
                 <section>
                     <h4>Em Andamento</h4>
                     <p>{emAndamento}</p>
+                    <Brightness1Icon className={styles.iconEmAndamento} sx={{ fontSize: 25, color: '#2B7FFF'}} />
+
                 </section>
 
                 <section>
                     <h4>Gravidade Alta</h4>
                     <p>{gravidadeAlta}</p>
+                    <AlertTriangle size={25} color="#f30000"/>
                 </section>
             </div>
             {/* ------------------------------------ */}
 
             {/* Dashboard Ações */}
             <div className={styles.dashboardActions}>
-                <button>
+                <button className={styles.buttonNovaDenuncia}>
+                    <PlusCircle size={45} color="#fd7608" />
                     <h4>Nova Denúncia</h4>
                     <p>Registrar um novo incêndio</p>
                 </button>
 
-                <button>
+                <button className={styles.buttonMinhasDenuncias}>
+                    <FileText size={45} color="#3f7cd8" />
                     <h4>Minhas Denúncias</h4>
                     <p>Ver todas as denúncias</p>
                 </button>
